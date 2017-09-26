abf9e576a6a7c41cc2a516db8d4551f80555ba0d
BlackIce21 <blackice9107@gmail.com>
2017-09-26 16:34:04 +0530

    added new files alt branch

--- script1.js
+++ script1.js
@@ -1,5 +1,32 @@
-window.onload = function(){
+window.onload = function() {
+  var parentDiv = document.getElementsByClassName("demo");
+  var counter = 1;
   var btn = document.getElementById("btn");
+  var container = document.getElementById("info-here");
+  btn.addEventListener("click", function() {
+    var req = new XMLHttpRequest();
+    req.open('GET', 'https://learnwebcode.github.io/json-example/animals-' + counter + '.json');
+    req.onload = function() {
+      var data = JSON.parse(req.responseText);
+      renderHTML(data);
+    };
+    req.send();
+    counter++;
+    if(counter > 3){
+      btn.style.display = "none";
+    }
+  });
 
-  btn.onClick();
+  function renderHTML(data) {
+    var htmlstring = "";
+
+    for(var i=0;i<data.length;i++){
+      htmlstring += "<p>" + data[i].name + " is a " + data[i].species + " and likes to eat ";
+      for(var j=0;j<data[i].foods.likes.length;j++){
+        htmlstring += data[i].foods.likes[j];
+      }
+      htmlstring += ".</p>";
+    }
+    container.innerHTML = htmlstring;
+  }
 };
