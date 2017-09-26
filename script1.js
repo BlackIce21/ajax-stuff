c8b8b41857dccc906a79c3c4f158afd0c83af5a9
BlackIce21 <blackice9107@gmail.com>
2017-09-26 17:02:10 +0530

    Done with AJAX

--- script1.js
+++ script1.js
@@ -7,23 +7,42 @@ window.onload = function() {
     var req = new XMLHttpRequest();
     req.open('GET', 'https://learnwebcode.github.io/json-example/animals-' + counter + '.json');
     req.onload = function() {
+      if(req.status >=200 && req.status < 404){
       var data = JSON.parse(req.responseText);
       renderHTML(data);
+    }else{
+      console.log("connected but can't retrieve");
+    }
+    };
+    req.onerror = function(){
+      console.log("Connection error");
     };
     req.send();
     counter++;
-    if(counter > 3){
-      btn.style.display = "none";
+    if (counter > 3) {
+      btn.disabled = "true";
     }
   });
 
   function renderHTML(data) {
     var htmlstring = "";
 
-    for(var i=0;i<data.length;i++){
+    for (var i = 0; i < data.length; i++) {
       htmlstring += "<p>" + data[i].name + " is a " + data[i].species + " and likes to eat ";
-      for(var j=0;j<data[i].foods.likes.length;j++){
-        htmlstring += data[i].foods.likes[j];
+      for (var j = 0; j < data[i].foods.likes.length; j++) {
+        if (j == 0) {
+          htmlstring += data[i].foods.likes[j];
+        } else {
+          htmlstring += " and " + data[i].foods.likes[j];
+        }
+      }
+      htmlstring += " and dislikes ";
+      for (var k = 0; k < data[i].foods.dislikes.length; k++) {
+        if (k == 0) {
+          htmlstring += data[i].foods.dislikes[k];
+        } else {
+          htmlstring += " and " + data[i].foods.dislikes[k];
+        }
       }
       htmlstring += ".</p>";
     }
